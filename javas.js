b8307ae1d1d1a2eacc3ed221583277eb5ee31573
FnA-v1 <faaiznurazka@gmail.com>
2026-06-22 18:09:24 +0700

    Add files via upload

---
+++ javas.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const loginForm = document.getElementById('loginForm');
+    const passwordInput = document.getElementById('password');
+    const togglePasswordBtn = document.getElementById('togglePassword');
+
+    togglePasswordBtn.addEventListener('click', () => {
+        const isPassword = passwordInput.getAttribute('type') === 'password';
+        passwordInput.setAttribute('type', isPassword ? 'text' : 'password');
+        
+        togglePasswordBtn.textContent = isPassword ? 'Hide' : 'Show';
+    });
+
+    loginForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        
+        const email = document.getElementById('email').value;
+        const password = passwordInput.value;
+
+        console.log('Logging in with:', { email, password });
+
+        if(email === "admin@gmail.com" && password === "123"){
+            window.location.href="/html/home.html";
+        } else {
+            alert("email atau password salah!");
+        }
+    });
+});
